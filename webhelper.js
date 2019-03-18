c1776f25265c9d239306d0e4f37353dbb372bddc
bmoreini <42300498+bmoreini@users.noreply.github.com>
2019-03-18 13:14:40 -0400

    Many additions

--- webhelper.js
+++ webhelper.js
@@ -1,31 +1,55 @@
-window.onload = start;
+window.onload = setup;
 
 var turn=0;
+var colorsPicked=[];
 var colors=[], code=[], guess=[], feedback=[];
 colors = ["r","b","g","w","c","y"];
 var thisTurn = [], turnRecords = [];
-
-function start() {
-	setup();
-}
+var board = document.getElementById("board");
+var title = document.getElementById("title");
+var buttonElement = document.getElementById("submit-guess");
+var myPicks = document.getElementById("colors");
 
 function setup() {
-	var board = document.getElementById("board");
 	board.removeChild(board.childNodes[0]);
-	var title = document.getElementById("title");
 	title.innerHTML = "Mastermind!";
-	boardReset("Here are instructions.");
-	var buttonElement = document.getElementById("submit-guess");
+	boardReset("<p class=\"clicker\">Click for instructions.</p><p>Press play button below to begin.</p>");
+	board.setAttribute("onclick","instructions()");
+	myPicks.classList.add("hide");
 	buttonElement.onclick = function () {
 		code=startGame();
 	}
+}
+
+function instructions() {
+	var howTo="Pick four colors by clicking on the four circles you'll see later, then press the Play button to get feedback.  Picking magenta on the first circle quits the game. You can choose more than one of each color, but picking all the same color is wasteful.\n\nThe game will respond with black circles for each right color in the right place, and white circles for right colors in the wrong place. The position of the black or white circles does not mean anything."
+	alert(howTo);
+}
+
+function fourPicked(sid) {
+	var colorPick = document.getElementById(sid);
+	var included=false;
+	colorPick.className = '';
+	colorPick.classList.add(colorPick.value);
+	for (var val=0; val<colorsPicked.length;val++){
+		if (colorsPicked[val]==sid){
+			included=true;
+		}
+	}
+	if (included==false) {
+		colorsPicked.push(sid)
+	}	
+	if (colorsPicked.length==4)
+		Button=document.getElementById("submit-guess");
+		Button.classList.add("fourok");
 }
 
 function startGame() {
 	code=setCode(colors);
 	var buttonElement = document.getElementById("submit-guess");
 	board.removeChild(board.childNodes[1]);
-	boardReset("Code Is Set up!<br /><br />\nPick your first choices.\n Magenta quits.");
+	myPicks.className = "";
+	boardReset("Code Is Set up!<br /><br />\nPick four choices.\n <span class=\"m\">Magenta</span> quits.");
 	for (i=0;i<4;i++) {
 		g=document.getElementById(i);
 		guess[i]=g.options[g.selectedIndex].value;
@@ -63,8 +87,8 @@ function masterMain(code,guess,turn){
 		board.style.backgroundImage="url('https://media.giphy.com/media/13vfiD0VBeksYE/giphy.gif')";
 		newGame();
 	}
-	else if(guess[0]=="q"){
-	  boardReset("Quitter! Play again?");
+	else if(guess[0]=="m"){
+	  boardReset("Quitter! Play again? Press play.");
 		board.style.backgroundColor = "red";
 		board.style.color = "white";
 		newGame();
